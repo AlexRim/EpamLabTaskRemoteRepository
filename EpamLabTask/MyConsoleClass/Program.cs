13e8bebe8b6af5f10f84b5c5f70b74dffded411d
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-18 20:58:55 +0300

    Task b) Ready

--- EpamLabTask/MyConsoleClass/Program.cs
+++ EpamLabTask/MyConsoleClass/Program.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace MyConsoleClass
 {
     class Program
     {
+
+
+        private static void AddTwoIntegersMethod()
+        {
+            int x = 0;
+            int y = 0;
+
+            while (true)
+            {
+             
+                WriteLine("Input first value:");
+                var integer1 = ReadLine();
+                WriteLine("Input second value:");
+                var integer2 = ReadLine();
+
+                if (Int32.TryParse(integer1, out x) && Int32.TryParse(integer2, out y))
+                {
+                    int sum = y + x;
+                    WriteLine("Sum={0}", sum);
+                    break;
+                }
+                else
+                {
+                    WriteLine("One of params has wrong format!"+"\n");
+                    continue;
+                }
+            }
+
+
+
+        }
+
+
         static void Main(string[] args)
         {
+
+            try
+            {
+                AddTwoIntegersMethod();
+                ReadKey();
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+
         }
     }
 }
