6d99cfff22beaa76cfcfc60afc9ecc598d710fae
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-18 22:42:25 +0300

    Task e) Ready

--- EpamLabTask/MyConsoleClass/Program.cs
+++ EpamLabTask/MyConsoleClass/Program.cs
@@ -13,6 +13,46 @@ namespace MyConsoleClass
     {
 
 
+
+
+        private static void UseLibraryMethods()
+        {
+            var calc = new MyCalcClass();
+
+            int x = 0;
+            int y = 0;
+            while (true)
+            {        
+                WriteLine("Input first value:");
+                var integer1 = ReadLine();
+                WriteLine("Input second value:");
+                var integer2 = ReadLine();
+
+                if (Int32.TryParse(integer1, out x) && Int32.TryParse(integer2, out y))
+                {
+
+                    WriteLine("Sum={0}", calc.Add(x, y));
+                    try
+                    {
+                        WriteLine("Division Result={0}", calc.Divide(x, y));
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        WriteLine(ex.Message);
+                    }
+                    WriteLine("Multyply Result={0}", calc.Multyply(x, y));
+                    WriteLine("Substract Result={0}", calc.Substract(x, y));
+                    break;
+                }
+                else
+                {
+                    WriteLine("One of params has wrong format!" + "\n");
+                    continue;
+                }
+
+            }
+        }
+
         private static void AddTwoIntegersMethod()
         {
             int x = 0;
@@ -53,8 +93,7 @@ namespace MyConsoleClass
 
             try
             {
-                //  AddTwoIntegersMethod();
-                WriteLine(m.Divide(17,0));
+                UseLibraryMethods();
          
             }
             catch (Exception ex)
