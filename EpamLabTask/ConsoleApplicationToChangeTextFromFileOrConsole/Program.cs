1cb86c5c6a0b97edd44fe94635da230cba594860
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-22 21:30:41 +0300

    Task2 is Ready and works, but needs refact.

--- EpamLabTask/ConsoleApplicationToChangeTextFromFileOrConsole/Program.cs
+++ EpamLabTask/ConsoleApplicationToChangeTextFromFileOrConsole/Program.cs
@@ -1,58 +1,63 @@
 ﻿using System;
 using System.IO;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Console;
 
 namespace ConsoleApplicationToChangeTextFromFileOrConsole
 {
     class Program
     {
 
-
         private static string ReadTextFromFile(string path) => File.ReadAllText(path);
 
-        private static string ChangeUpperCaseLiteralsToLowerCase(string text)
+        private static string[] SplitTextOnSentences(string text) => Regex.Split(text, @"(?<=['""A-Za-z0-9][\.\!\?])\s+(?=[A-Z])");
+
+        private static List<StringBuilder> ChangeUpperCaseLiteralsToLowerCase(string [] splitTextArray)
         {
-            char[] myCharArray = text.ToCharArray();
-
-            for (int i = 0; i < myCharArray.Length; i++)
+            var strBuildList = new List<StringBuilder>();
+            foreach   (var i in splitTextArray)
             {
-                if (char.IsUpper(myCharArray[i]))
+                var sr = new StringBuilder(i);              
+                for (int j = 0; j < sr.Length; j++)
                 {
-                    myCharArray[i] = char.ToLower(myCharArray[i]);
+                    if(char.IsUpper(sr[j]))
+                    {
+                        sr[j] = char.ToLower(i[j]);
+                    }
                 }
+                sr.Append("\n");
+                strBuildList.Add(sr);
             }
-          string str = new string(myCharArray);
-          return str;
-        }
-
-        private static void AddNewLine(string text)
-        {
-            var st = new StringBuilder(text);
-
-            for(int i=0; i<st.Length;i++)
-            {
-                if(st[i]=='.')
-                {
-                   
-                }
-            }
-
+            return strBuildList;
         }
 
 
+        private static string AddCurrentDateTime(List<StringBuilder> list)
+        {
+            string newText = null;
 
+            foreach(var i in list)
+            {
+                newText +="["+DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss:fff")+"] "+i.ToString();
+            }
 
-
+            return newText;
+        }
 
 
         static void Main(string[] args)
         {
-            string str = ReadTextFromFile(@".\TextFile.txt");
-            Console.WriteLine(ChangeUpperCaseLiteralsToLowerCase(str));
+            string text = ReadTextFromFile(@".\TextFile.txt");
+            string[] strArr = SplitTextOnSentences(text);
+            var listStrBuilder = ChangeUpperCaseLiteralsToLowerCase(strArr);
+            var finalString = AddCurrentDateTime(listStrBuilder);
+            WriteLine(finalString);
+
+
             Console.ReadKey();
         }
     }
