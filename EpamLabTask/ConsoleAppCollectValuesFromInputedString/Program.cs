a314086672f8b0b4fa8c08fd0d13864d03a727f7
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-23 19:57:49 +0300

    Add new project:task 3

---
+++ EpamLabTask/ConsoleAppCollectValuesFromInputedString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCollectValuesFromInputedString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
