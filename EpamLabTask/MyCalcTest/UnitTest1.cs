198de011f76c00ffff1e8bbe1aed79c69f436901
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-25 20:16:17 +0300

    Tests for divide and multiply actions were added;

--- EpamLabTask/MyCalcTest/UnitTest1.cs
+++ EpamLabTask/MyCalcTest/UnitTest1.cs
@@ -7,11 +7,16 @@ namespace MyCalcTest
     [TestClass]
     public class UnitTest1
     {
+
+
+
+
+
         public TestContext TestContext { get; set; }
 
 
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "dataAdd.csv", "dataAdd#csv",
-         DataAccessMethod.Sequential), 
+         DataAccessMethod.Sequential),
          DeploymentItem("dataAdd.csv"), TestMethod]
         public void Add_xy_ReturnResultZ()
         {
@@ -19,7 +24,7 @@ namespace MyCalcTest
             int x = Int32.Parse(this.TestContext.DataRow[0].ToString());
             int y = Int32.Parse(this.TestContext.DataRow[1].ToString());
             int z = Int32.Parse(this.TestContext.DataRow[2].ToString());
-            Assert.AreEqual(z, calc.Add(x,y));
+            Assert.AreEqual(z, calc.Add(x, y));
         }
 
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "dataSub.csv", "dataSub#csv",
@@ -33,6 +38,47 @@ namespace MyCalcTest
             int z = Int32.Parse(this.TestContext.DataRow[2].ToString());
             Assert.AreEqual(z, calc.Substract(x, y));
         }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "dataMult.csv", "dataMult#csv",
+     DataAccessMethod.Sequential),
+     DeploymentItem("dataMult.csv"), TestMethod]
+        public void Mult_xy_ReturnResultZ()
+        {
+            var calc = new MyCalcClass();
+            int x = Int32.Parse(this.TestContext.DataRow[0].ToString());
+            int y = Int32.Parse(this.TestContext.DataRow[1].ToString());
+            int z = Int32.Parse(this.TestContext.DataRow[2].ToString());
+            Assert.AreEqual(z, calc.Multiply(x, y));
+
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "dataDivideByzero.csv", "dataDivideByzero#csv",
+        DataAccessMethod.Sequential),
+        DeploymentItem("dataDivideByzero.csv")]
+        [TestMethod, ExpectedException(typeof(System.DivideByZeroException))]
+        public void DivideByZeroExpectException()
+        {
+            var calc = new MyCalcClass();
+            int x = Int32.Parse(this.TestContext.DataRow[0].ToString());
+            int y = Int32.Parse(this.TestContext.DataRow[1].ToString());
+            calc.Divide(x, y);
+
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "dataDivide.csv", "dataDivide#csv",
+DataAccessMethod.Sequential),
+DeploymentItem("dataDivide.csv"), TestMethod]
+        public void Divide_xy_ReturnResultZ()
+        {
+            var calc = new MyCalcClass();
+            int x = Int32.Parse(this.TestContext.DataRow[0].ToString());
+            int y = Int32.Parse(this.TestContext.DataRow[1].ToString());
+            int z = Int32.Parse(this.TestContext.DataRow[2].ToString());
+            Assert.AreEqual(z, calc.Divide(x, y));
+
+        }
+
+
 
 
     }
