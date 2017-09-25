4b8dfcb9112b3887c8e998b2baa66e0259e51c2b
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-25 21:55:14 +0300

    Begin task 4
    Задания к модулю Operations
    1. Реализовать библиотеку, которая будет решать квадратные и линейные уравнения. Добавить в солюшн консольное приложение, которое будет работать с этой библиотекой:
    a. Пользователь с консоли выбирает тип уравнения и вводит коэффициенты
    b. В формате целого числа, либо двух знаков после запятой в случае вещественного, пользователю выводятся корни его уравнения
    c. Обработать случаи некорректного ввода коэффициентов и дожидаться корректного ввода
    d. После каждого уравнения добавлять в лог-файл запись о том, какое уравнение решалось и какие корни получились. Также логировать факты ввода пользователем некорректных данных (коэффициентов).
    2. Добавить в библиотеку из Задания 3 метод который будет умножать две матрицы (не обязательно квадратные).
    a. Данные считать из файла.
    b. Имя файла хранить в конфигурационном файле.
    c. Реализовать вывод результата на консоль в формате до 2 знаков после запятой.

---
+++ EpamLabTasksPart2/QuadraticEquationLibrary/GetRoots.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using static System.Math;
+
+namespace QuadraticEquationLibrary
+{
+    public class GetRoots
+    {
+        private double a;
+
+        private double b;
+
+        private double c;
+
+        private double [] qadraticRoots;
+
+        private double rootLine;
+
+        public double[] QadraticRoots { get => qadraticRoots; }
+
+        public double RootLine { get => rootLine; }
+
+  
+        public GetRoots(double a, double b, double c)
+        {
+            if(a==0 && b==0)
+            {
+                throw new NotTheEquationException("First two parameters cant be equal 0!");
+            }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            GetRootsIfQuadratic();
+
+        }
+
+        public GetRoots(double b, double c)
+        {
+            if(b==0)
+            {
+                throw new NotTheEquationException("First parameter cant be equal 0!");
+            }
+            this.b=b;
+            this.c=c;
+            GetRootsIfLine();
+        }
+
+        private void GetRootsIfQuadratic()
+        {
+            double D = Pow(this.b, 2) - 4 * this.a * this.c;
+            if(D<0)
+            {
+                throw new NegativeDiscriminantException("Discriminant<0 , there are no roots!");
+            }
+            else if(D>=0)
+            {
+               qadraticRoots = new double[2];
+               qadraticRoots[0] = (-this.b + Sqrt(D)) / 2 * this.a;
+               qadraticRoots[1]= (-this.b + Sqrt(D)) / 2 * this.a;
+            }
+        
+        }
+            
+         private void GetRootsIfLine()
+            {
+            //  bx + c = 0;
+
+            this.rootLine = -c / b;
+              
+
+            }
+
+
+
+
+    }
+}
---
+++ EpamLabTasksPart2/QuadraticEquationLibrary/NotTheEquationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadraticEquationLibrary
+{
+    class NotTheEquationException : Exception
+    {
+            public NotTheEquationException() { }
+            public NotTheEquationException(string message) : base(message) { }
+            public NotTheEquationException(string message, Exception ex) : base(message) { }
+    }
+}
