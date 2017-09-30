f8c1b0972cdd12dbbef9588338fb22425ea87af0
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-30 19:16:32 +0300

    BeginTask2

---
+++ EpamLabTasksPart2/TaskWithPointAndLinq/MyPoint.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskWithPointAndLinq
+{
+    class MyPoint
+    {
+        private double x;
+
+        private double y;
+
+
+
+        public double X { get => x; set => x = value; }
+
+        public double Y { get => y; set => y = value; }
+
+
+
+        public MyPoint() { }
+
+
+
+        public MyPoint(double x, double y)
+
+        {
+
+            this.x = x;
+
+            this.y = y;
+
+
+
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if(obj==null)
+            {
+                return false;
+            }
+
+            MyPoint p =obj as MyPoint;
+            if ((System.Object)p == null)
+            {
+               return false;
+            }
+
+            return (x==p.x)&&(y==p.y);
+        }
+
+
+
+        public override string ToString()
+
+        {
+
+            return "X=" + X + " " + "Y=" + Y;
+
+        }
+    }
+}
