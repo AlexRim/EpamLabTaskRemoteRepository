920cd3937514b53f51f81e566794f7104482909c
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-30 20:34:15 +0300

    test overriding method Equals() in MyPoint class

--- EpamLabTasksPart2/TaskWithPointAndLinq/Program.cs
+++ EpamLabTasksPart2/TaskWithPointAndLinq/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace TaskWithPointAndLinq
 {
@@ -10,6 +11,15 @@ namespace TaskWithPointAndLinq
     {
         static void Main(string[] args)
         {
+            MyPoint myPoint = new MyPoint(1, 1);
+            MyPoint myPoint1 = new MyPoint(1, 1);
+
+            WriteLine(myPoint.Equals(myPoint1));
+
+            ReadKey();
+
+
+            
         }
     }
 }
